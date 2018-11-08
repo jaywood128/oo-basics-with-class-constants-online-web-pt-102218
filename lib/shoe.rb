5f823da67df1a6f49d275bbc7b501247e4b36014
Johnathon Wood <jaywood128@gmail.com>
2018-11-08 16:07:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :material, :condition
+  attr_reader :brand, :size
 
   BRANDS = ["Uggs", "Rainbow"]
 
@@ -12,5 +12,7 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  def size=(size)
+    @size = size
+    BRANDS << size
 end
